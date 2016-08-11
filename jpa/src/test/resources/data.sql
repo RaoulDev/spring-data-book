c4fd8253bdc6d30df2f406233b9f4a2f7a9166f3
RaoulDev <raoul.alexie@googlemail.com>
2016-08-11 15:32:14 +0200

    Update data.sql
    Do you set on purpose the second address entry with the same customer_id as the first address entry?

--- jpa/src/test/resources/data.sql
+++ jpa/src/test/resources/data.sql
@@ -3,7 +3,7 @@ insert into Customer (id, email, firstname, lastname) values (2, 'carter@dmband.
 insert into Customer (id, email, firstname, lastname) values (3, 'boyd@dmband.com', 'Boyd', 'Tinsley');
 
 insert into Address (id, street, city, country, customer_id) values (1, '27 Broadway', 'New York', 'United States', 1);
-insert into Address (id, street, city, country, customer_id) values (2, '27 Broadway', 'New York', 'United States', 1);
+insert into Address (id, street, city, country, customer_id) values (2, '27 Broadway', 'New York', 'United States', 2);
 
 insert into Product (id, name, description, price) values (1, 'iPad', 'Apple tablet device', 499.0);
 insert into Product (id, name, description, price) values (2, 'MacBook Pro', 'Apple notebook', 1299.0);
